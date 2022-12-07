30698b0dae742107c947cfd7726a8746d75f7a74
Glenn McFetridge <glennmcfetrg@gmail.com>
2022-12-07 23:47:59 +0000

    feat: create catcard

---
+++ components/cards/cat/Cat.mocks.ts
@@ -0,0 +1,13 @@
+import { ICat } from './Cat';
+
+const base: ICat = {
+  tag: 'Felines',
+  title: `What's new in Cats`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Glenn',
+  time: '2h ago',
+};
+
+export const mockCatProps = {
+  base,
+};
