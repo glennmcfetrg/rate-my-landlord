7a31c0d09d8d715e5edc81c6fe281a8735dca0cb
Glenn McFetridge <glennmcfetrg@gmail.com>
2022-12-10 00:38:06 +0000

    feat: adding placeholder logo and language toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import Search from '../components/utility/search/Search';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
+  const { locale } = useRouter();
   return (
-    <section>
-      logo
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
+      <Image
+        src="/Google.png"
+        alt="Google Logo"
+        width={250}
+        height={85}
+        priority
+      />
       <Search />
+      <p>
+        RateMyLandLord offered in:{' '}
+        <Link
+          href="/"
+          locale={locale === 'en' ? 'fr' : 'en'}
+          className="underline text-blue-600"
+        >
+          Français
+        </Link>
+      </p>
     </section>
   );
 };
